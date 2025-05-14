7f9163cad1f2774d590056dab7d0c904296c8668
Shunsuke Fujinawa <shun26497204@gmail.com>
2025-05-14 23:25:14 +0900

    first commit

--- app/hooks/useThemeColor.ts
+++ app/hooks/useThemeColor.ts
@@ -3,8 +3,8 @@
  * https://docs.expo.dev/guides/color-schemes/
  */
 
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Colors } from '@/app/constants/Colors';
+import { useColorScheme } from '@/app/hooks/useColorScheme';
 
 export function useThemeColor(
   props: { light?: string; dark?: string },
