bd97b2be8f744563dc2842fda52e7c07501ab776
Shunsuke Fujinawa <shun26497204@gmail.com>
2025-05-17 10:48:09 +0900

    change: app配下に移動

---
+++ app/index.tsx
@@ -0,0 +1,36 @@
+import { registerRootComponent } from "expo";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>麻雀 点数計算アプリ</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    fontSize: 36,
+    color: "#38434D",
+  },
+});
+
+registerRootComponent(Page);
